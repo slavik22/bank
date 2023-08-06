255ceb3b0b0d6d93afa01ef32af95081c2fe80f2
Yaroslav Fedyna <fedynaslava@gmail.com>
2023-08-06 09:56:59 +0300

    Accounts endpoints added

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bank/api"
+	db "bank/db/sqlc"
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgres://postgres:pass@localhost:5432/bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("Cannot open db connection ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+
+	if err != nil {
+		log.Fatal("Cannot start server ", err)
+	}
+}
